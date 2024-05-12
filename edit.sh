9e4c84af5948f07102c26b44df1cf0ee542968ab
shragsir <32632850-shragsir@users.noreply.replit.com>
2024-05-12 05:52:15 +0000

    update

---
+++ edit.sh
@@ -0,0 +1,103 @@
+git add .
+git commit -m "update"
+git push origin main
+git pull origin main
+
+echo "   {-}"
+sleep 0.2
+
+clear
+echo "  { / }"
+sleep 0.2
+
+clear
+echo " {  |  }"
+sleep 0.2
+
+clear
+echo "{  {\}  } "
+sleep 0.2
+
+clear
+echo "   {-}"
+sleep 0.2
+
+clear
+echo "  { / }"
+sleep 0.2
+
+clear
+echo " {  |  }"
+sleep 0.2
+
+clear
+echo "{  {\}  } "
+sleep 0.2
+
+clear
+echo "   {-}"
+sleep 0.2
+
+clear
+echo "  { / }"
+sleep 0.2
+
+clear
+echo " {  |  }"
+sleep 0.2
+
+clear
+echo "{  {\}  } "
+sleep 0.2
+
+clear
+echo "   {-}"
+sleep 0.2
+
+clear
+echo "  { / }"
+sleep 0.2
+
+clear
+echo " {  |  }"
+sleep 0.2
+
+clear
+echo "{  {\}  } "
+sleep 0.2
+
+clear
+echo "   {-}"
+sleep 0.2
+
+clear
+echo "  { / }"
+sleep 0.2
+
+clear
+echo " {  |  }"
+sleep 0.2
+
+clear
+echo "{  {\}  } "
+sleep 0.2
+
+clear
+echo "   {-}"
+sleep 0.2
+
+clear
+echo "  { / }"
+sleep 0.2
+
+clear
+echo " {  |  }"
+sleep 0.2
+
+clear
+echo "{  {\}  } "
+sleep 0.2
+
+clear
+
+exit
