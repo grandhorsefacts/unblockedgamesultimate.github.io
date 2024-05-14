4556acf72272014906aae4aca3865a2876180c27
shragsir <32632850-shragsir@users.noreply.replit.com>
2024-05-14 18:38:15 +0000

    update

--- edit.sh
+++ edit.sh
@@ -117,4 +117,36 @@ sleep 0.2
 
 clear
 
-exit
+echo "Enable auto-updates? [Yn]:"
+read message
+
+if [ "$message" = "y" ]; then
+  echo "Enabling auto-updates..."
+  sleep 1
+
+  chmod +x auto-updates.sh
+  ./auto-updates.sh
+elif [ "$message" = "n" ]; then
+  echo "Would you like to run process again? [Yn]:"
+  read message
+  
+  if [ "$message" = "y" ]; then
+    echo "Restarting..."
+    sleep 1
+    
+    ./edit.sh
+  elif [ "$message" = "n" ]; then
+    echo "Ending process..."
+    sleep 1
+    clear
+    exit
+  else
+    echo "Invalid response. Ending process..."
+    sleep 1
+  fi
+else
+  echo "Invalid response. Ending process..."
+  sleep 1
+  clear
+  exit
+fi
