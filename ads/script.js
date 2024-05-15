bc1536fa24b7aa1c1eea93aeb122a3c9f17824cb
shragsir <32632850-shragsir@users.noreply.replit.com>
2024-05-15 19:15:09 +0000

    update

---
+++ ads/script.js
@@ -0,0 +1,23 @@
+var adFiles = [
+    "dev-pirates-1.html",
+    "dev-pirates-2.html",
+    "premium-1.html",
+    "premium-2.html",
+    "premium-3.html"
+];
+
+function getRandomAd() {
+    var randomIndex = Math.floor(Math.random() * adFiles.length);
+    return adFiles[randomIndex];
+}
+
+function displayAd(adPath) {
+    console.log("Advertisment:", adPath);
+}
+
+function runRandomAd() {
+    var randomAd = getRandomAd();
+    displayAd(randomAd);
+}
+
+runRandomAd();
